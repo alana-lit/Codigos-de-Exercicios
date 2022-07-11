8bb225fe0da7ae53d5aef21b2da0419c1b1c49fb
Alana Lima Tavares <alanapfss@gmail.com>
2022-07-11 11:59:50 -0300

    Novas atividades utilizando Random: ArrayDeNumeros e MatrizAleatoria

---
+++ src/br/com/dio/arrays/ArrayDeNumeros.java
@@ -0,0 +1,28 @@
+package br.com.dio.arrays;
+
+import java.util.Random;
+
+public class ArrayDeNumeros {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[20];
+        Random r = new Random();
+
+        for(int i = 0; i < 20; i++){
+            //numeros[i] = r.nextInt(100);
+            int numeroQualquer = r.nextInt(100);
+            numeros[i] = numeroQualquer;
+        }
+
+        System.out.println("Vetor de numeros aleatorios: ");
+        for (int numeroQlaquer: numeros) {
+            System.out.print(numeroQlaquer + " ");
+        }
+
+        System.out.println("\nTriplo dos numeros aleatorios: ");
+        for (int numeroQlaquer: numeros) {
+            System.out.print(numeroQlaquer*3 + " ");
+        }
+
+    }
+}
---
+++ src/br/com/dio/arrays/MatrizAleatoria.java
@@ -0,0 +1,32 @@
+package br.com.dio.arrays;
+
+import java.util.Random;
+
+public class MatrizAleatoria {
+
+    public static void main(String[] args) {
+        int[][] matrizAleatoria = new int[4][4];
+        Random r = new Random();
+
+        for (int i = 0; i < matrizAleatoria.length; i++){
+            for (int j = 0; j < matrizAleatoria.length; j++) {
+                int numero = r.nextInt(9);
+                matrizAleatoria[i][j] = numero;
+            }
+        }
+
+        System.out.println("Matriz de numeros aleatorios: ");
+        /*for (int[] linha: matrizAleatoria) {
+            for (int coluna: linha) {
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }*/
+        for (int i = 0; i < matrizAleatoria.length; i++){
+            for (int j = 0; j < matrizAleatoria.length; j++) {
+                System.out.print(matrizAleatoria[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
